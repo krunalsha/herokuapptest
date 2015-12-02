5a8c682880de9d5bc624aff0cc487216b07aa681
Krunal Shah <krunalsha@cybage.com>
2015-12-02 18:42:44 +0530

    updated server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('STAGING, Hello World');
+	response.end('STAGING change, Hello World');
 });
 
 var port = Number(process.env.PORT || 3000)
