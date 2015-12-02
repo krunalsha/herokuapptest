bff602de5bab66d44374ca16a5f2c799f0f15e87
Krunal Shah <krunalsha@cybage.com>
2015-12-02 16:03:56 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+var http = require('http');
+
+var server = http.createServer(function(request, response){
+	response.writeHead(200, {'Content-Type': 'text/html'});
+	response.end('Krunal Shah says, Hello World');
+});
+
+var port = Number(process.env.PORT || 3000)
+server.listen(port, function() {
+	console.log("Heroku app test is running on port ==>" + port); 
+}); 
