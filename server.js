f121ead7dc77b3482df42286f48efb53eb8ce59d
Krunal Shah <krunalsha@cybage.com>
2015-12-02 16:07:11 +0530

    updated greet message of staging

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http');
 
 var server = http.createServer(function(request, response){
 	response.writeHead(200, {'Content-Type': 'text/html'});
-	response.end('Krunal Shah says, Hello World');
+	response.end('STAGING, Hello World');
 });
 
 var port = Number(process.env.PORT || 3000)
